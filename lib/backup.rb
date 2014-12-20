6a12ac94189c2f6a17b7141cb8926051240df19c
Ricardo Trindade <ricardo.silva.trindade@gmail.com>
2014-12-20 14:31:05 +0000

    added Heroku Postgres backups to new version of the backup gem

--- lib/backup.rb
+++ lib/backup.rb
@@ -79,6 +79,7 @@ module Backup
     autoload :Riak,       File.join(DATABASE_PATH, 'riak')
     autoload :OpenLDAP,   File.join(DATABASE_PATH, 'openldap')
     autoload :SQLite,     File.join(DATABASE_PATH, 'sqlite')
+    autoload :HerokuPostgreSQL, File.join(DATABASE_PATH, 'heroku_postgresql')    
   end
 
   ##
